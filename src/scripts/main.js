41d5cdcd7f68e1920ca677373142cab8f6212ca2
Oleksandr Kovalchuk <kov280396@gmail.com>
2024-12-07 18:54:09 +0200

    small improvements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,30 @@
 'use strict';
 
 const logo = document.querySelector('.logo');
-const messageDiv = document.createElement('div');
 
-document.body.appendChild(messageDiv);
-
-function handleSuccess(message) {
-  messageDiv.className = 'message';
-  messageDiv.textContent = message;
-}
-
-function handleError(errorMessage) {
-  messageDiv.className = 'message error-message';
-  messageDiv.textContent = errorMessage;
-}
-
-function handleClick() {
-  const promise1 = new Promise((resolve) => {
-    resolve('Promise was resolved!');
-  });
-
-  promise1.then(handleSuccess).catch(handleError);
-}
-
-logo.addEventListener('click', handleClick);
-
-const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject(new Error('Promise was rejected!'));
-  }, 3000);
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => resolve('Promise was resolved!'));
 });
 
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
+});
+
+const createMessageDiv = (message, isError = false) => {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+
+  if (isError) {
+    div.classList.add('error-message');
+  }
+
+  div.textContent = message;
+  document.body.appendChild(div);
+};
+
+const handleSuccess = (message) => createMessageDiv(message);
+const handleError = (error) => createMessageDiv(error.message, true);
+
+promise1.then(handleSuccess).catch(handleError);
 promise2.then(handleSuccess).catch(handleError);
